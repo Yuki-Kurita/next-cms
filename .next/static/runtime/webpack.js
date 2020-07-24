079c855e059d087c4f0dbcc418a8cc173ea30d00
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-07-24 20:08:09 +0900

    media cardコンポーネントの作成:sparkles:

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6c7b5612765760cb5b14";
+/******/ 	var hotCurrentHash = "1c2d284f6ad49c3d7f8a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
 import SearchAppBar from '../src/components/SearchAppBar'
-import Button from '@material-ui/core/Button';
+import MediaCard from '../src/components/MediaCard'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const Index = () => {
   return (
     <>
-    <SearchAppBar
-      color="inherit"
-    />
+    <SearchAppBar/>
+    <div className="articles">
+      <Grid container spacing={3}>
+        {/* 記事領域 */}
+        <Grid item xs={12} sm={8}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} lg={4}>
+              <MediaCard/>
+            </Grid>
+          </Grid>
+        </Grid>
+        {/* タグ領域 */}
+        <Grid item xs={12} sm={4}>
+          ここにタグを入れる
+        </Grid>
+      </Grid>
+      <style jsx>{`
+        .articles {
+          margin-top: 2em;
+          margin-left: 2em;
+          flex-grow: 1;
+        }
+      `}</style>
+    </div>
     </>
   )
 }
